46d6271c22263d11e3af05062101f53e83b8f9d4
Aleksander Dabrowski <aleks@kumulator.pl>
2010-11-30 00:11:00 +0100

    initial haml page added

--- bin/webapp.rb
+++ bin/webapp.rb
@@ -1,10 +1,13 @@
 require 'rubygems'
 require 'sinatra'
 require 'brb'
+require 'haml'
+
+set :views, File.dirname(__FILE__) + '/../templates'
 
 get '/' do
   server = BrB::Tunnel.create(nil, "brb://localhost:5556")
   res = server.diagnose_block
   EM.stop
-  res
+  haml :index
 end
